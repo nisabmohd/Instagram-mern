4101dff76f03c6c94ec485218c7da6c2d9dde908
nisabmohd <nisabmohd@gmail.com>
2022-10-21 02:12:25 +0530

    minor ui fixed

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -55,7 +55,7 @@ export const Profile = ({ post=true }) => {
           <p style={{ textAlign: 'center', marginTop: '4px', fontSize: '14px', fontWeight: 'bold', color: '#424141' }}>Highlights</p>
         </div>
         <div className="highlight-story" style={{ display: 'flex', flexDirection: 'column', marginRight: '35px' }}>
-          <div className="imageuser" style={{ width: '85px', height: '85px', borderRadius: '50%', border: '1px solid #c1c1c1', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '5px', }}>
+          <div className="imageuser" style={{ width: '82px', height: '82px', borderRadius: '50%', border: '1px solid #c1c1c1', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '5px', }}>
             <svg aria-label="Plus icon" class="_ab6-" color="#c7c7c7" fill="#c7c7c7" height="44" role="img" viewBox="0 0 24 24" width="44"><path d="M21 11.3h-8.2V3c0-.4-.3-.8-.8-.8s-.8.4-.8.8v8.2H3c-.4 0-.8.3-.8.8s.3.8.8.8h8.2V21c0 .4.3.8.8.8s.8-.3.8-.8v-8.2H21c.4 0 .8-.3.8-.8s-.4-.7-.8-.7z"></path></svg>
           </div>
           <p style={{ textAlign: 'center', marginTop: '4px', fontSize: '14px', fontWeight: 'bold', color: '#424141' }}>New</p>
