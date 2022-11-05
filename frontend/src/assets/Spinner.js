3e1b04cd2be99128be1e8343fbe3b4c80163a1a6
nisabmohd <nisabmohd@gmail.com>
2022-11-05 22:50:58 +0530

    story and spinner css fix

--- frontend/src/assets/Spinner.js
+++ frontend/src/assets/Spinner.js
@@ -2,5 +2,5 @@ import React from "react";
 import loader from "./loader.gif";
 
 export const Spinner = () => {
-  return <img style={{margin:'auto',width:'20px',marginTop:'35px'}} src={loader} alt="" />;
+  return <div style={{width:'100%',display:'flex',alignItems:'center',justifyContent:'center'}}><img style={{margin:'auto',width:'20px',marginTop:'35px'}} src={loader} alt="" /></div>
 };
